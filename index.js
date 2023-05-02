e8d93c7f42655dfa1984db95b7a3e53ba3c9155d
Gina <gmroscioli@gmail.com>
2023-05-02 17:18:37 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record) {
+    const isWin = function (x) {
+        return x.result === "W";
+    };
+    let win = record.find(isWin);
+    if (win === undefined) {
+        return undefined;
+    }
+    else {   
+        return win.year;
+    }
+    
+}
